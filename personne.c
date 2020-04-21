69ca05b7423f8c77b6c9df8a8b910c66e1c8d3b3
nacib-mariem <rajianacib@gmail.com>
2020-04-21 10:30:39 -0700

    mariem

--- personne.c
+++ personne.c
@@ -16,9 +16,9 @@ void initialiser_personnage(personnage *p)
 p->position_personnage.x=0;
 p->position_personnage.y=180;
 
-char nomFich[50];
+char nomFich[99];
 int i;
-for(i=0;i<50;i++)
+for(i=0;i<99;i++)
 {
     sprintf(nomFich,"per/%d.png",i);
     p->tab[i]=IMG_Load(nomFich);
@@ -74,14 +74,12 @@ void animation_crouch (personnage *p)
 	p->Frame++;
 }
 
-/*
+
 //---------------------Animation de l'attack du personnage---------------------//
 
 void animation_hit (personnage *p)
 {
-int nb_frames_p=0;
-  
-	if(p->Frame<=45 || p->Frame>=60)
+	if(p->Frame<=45 || p->Frame>=55)
 		p->Frame=46;
 
 	p->Frame++;
@@ -101,16 +99,20 @@ void animation_slide (personnage *p)
 
 
 //---------------------animation du personnage en cas d'echouer---------------------//
+/*
 void animation_fail (personnage *p)
 {
 int nb_frames_p=0;
   
-	if(p->Frame<=61 || p->Frame>=74)
-		p->Frame=62;
+	if(p->Frame<=45 || p->Frame>=55)
+		p->Frame=46;
 
 	p->Frame++;
 	
 }*/
+
+
+
 
 
 
@@ -133,8 +135,9 @@ void deplacement_clavier(personnage *p,int clic)
             animation_right (p);
           }
             else if (clic==3)
-              {p->position_personnage.x += p->vitesse;
-               p->position_personnage.y -= p->vitesse;
+ 	{
+            p->position_personnage.x += p->vitesse;
+             p->position_personnage.y -= p->vitesse;
             
             animation_jump (p);
 
@@ -142,12 +145,11 @@ void deplacement_clavier(personnage *p,int clic)
                else if(clic==4)
                {
                  p->position_personnage.x += p->vitesse;
-                 p->position_personnage.y += p->vitesse;
+            p->position_personnage.y += p->vitesse;
             animation_crouch (p);
 
 
                }
-<<<<<<< HEAD
 
 		else if(clic==5)
                {
@@ -165,8 +167,6 @@ void deplacement_clavier(personnage *p,int clic)
 
 
                }
-=======
->>>>>>> 6eb245f1700661a0363f2fb02d7b302efeac1720
 }
 
 //---------------------Deplacement du personnage moyennant la sourie---------------------//
@@ -190,15 +190,15 @@ void deplacement_sourie (personnage *p, int clic)
           }
           else if(clic==3)
           {
-           p->position_personnage.x += p->vitesse;
-               p->position_personnage.y -= p->vitesse;
+            p->position_personnage.x += p->vitesse;
+            p->position_personnage.y -= p->vitesse;
+		  
             animation_jump (p);
           }
           else if(clic==4)
           {
-            
-                 p->position_personnage.x += p->vitesse;
-                 p->position_personnage.y += p->vitesse;
+            p->position_personnage.y += p->vitesse;
+            p->position_personnage.x += p->vitesse;
 		        animation_crouch (p);
           }
 
